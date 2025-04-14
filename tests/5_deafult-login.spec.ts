8c1352677c84ab958b75501856351541b30f4c12
Wojciech Sowa <w.sowa6@gmail.com>
2025-04-14 12:08:14 +0200

    first 5 tests
    init commit and first 5 tests

---
+++ tests/5_deafult-login.spec.ts
@@ -0,0 +1,23 @@
+import { test, expect } from '@playwright/test';
+import { chromium } from 'playwright';
+
+
+test('DEAFULT_LOGIN', async () => {
+    const browser = await chromium.launch({
+        headless: false,
+        slowMo: 500,
+        timeout: 0,
+    });
+    const page = await browser.newPage();
+    await page.goto('https://www.kurier-kwiatowy.pl/');
+    await page.locator('#CybotCookiebotDialogBodyButtonDecline').click();
+
+    await page.getByRole('link', { name: 'Zaloguj' }).click();
+    await page.getByRole('textbox', { name: 'E-mail(wymagane)' }).click();
+    await page.getByRole('textbox', { name: 'E-mail(wymagane)' }).fill('cozaidiota2137@proton.me');
+    await page.getByRole('textbox', { name: 'Hasło(wymagane)' }).click();
+    await page.getByRole('textbox', { name: 'Hasło(wymagane)' }).fill('utwurzSlineHaslo1@');
+    await page.getByRole('button', { name: 'zaloguj się »' }).click();
+
+    browser.close();
+});
